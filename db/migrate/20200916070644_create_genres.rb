ef9d8e910f6df69c1abaebfbc2c53fb2756a75e8
vagrant <vagrant@localhost.localdomain>
2020-09-17 12:01:27 +0000

    genre

--- db/migrate/20200916070644_create_genres.rb
+++ db/migrate/20200916070644_create_genres.rb
@@ -1,7 +1,7 @@
 class CreateGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :genres do |t|
-      t.name :string
+      t.string :name
       t.boolean :valid_flag, default: true
 
       t.timestamps
