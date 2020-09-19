c3891793b502ec0999e0a5fdc1745b727c623a66
vagrant <vagrant@localhost.localdomain>
2020-09-19 17:45:29 +0000

    customer_product

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -1,2 +1,19 @@
 class Customer::ProductsController < ApplicationController
+	before_action :authenticate_customer!, only: [:show]
+
+	def index
+		@genres = Genre.all
+		@products = Product.where(sales_status: true).page(params[:page]).per(8)
+	end
+
+	def show
+		@genres = Genre.all
+		@product = Product.find(params[:id])
+		@cart_item = CartItem.new
+	end
+
+	private
+	def product_params
+		parmas.require(:product).permit(:image ,:name, :description, :price_without_tax, :sales_status)
+	end
 end
