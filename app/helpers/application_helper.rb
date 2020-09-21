5802e4c55633395b94324f4512b2cba71ca6f8e6
Hiroki Isobe <vagrant@localhost.localdomain>
2020-09-21 07:58:17 +0000

    [Add]customerの注文機能

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  #税込価格表示のヘルパー(税別価格に10%の消費税を加算。小数点以下はfloorで切り捨て。)
+  #税別価格を税込価格表示へのヘルパー(税別価格に10%の消費税を加算。小数点以下はfloorで切り捨て。)
   def price_include_tax(price_without_tax) 
     (price_without_tax * 1.10).floor
   end
