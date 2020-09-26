db196d1dccb34603965de3f7d550b68f4aafb495
Hiroki Isobe <vagrant@localhost.localdomain>
2020-09-26 01:49:15 +0000

    [Fix]test結果から修正

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -17,8 +17,8 @@ class Admin::ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to admin_products_path(@product)
-      flash[:notice] = "投稿に成功しました"
+      redirect_to admin_product_path(@product)
+      flash[:notice] = "商品の追加に成功しました"
     else
       render :new
     end
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -3,7 +3,7 @@ class OrderProduct < ApplicationRecord
   belongs_to :order
 
   enum production_status: {
-    "制作不可": 0,
+    "着手不可": 0,
     "制作待ち": 1,
     "制作中": 2,
     "制作完了": 3
