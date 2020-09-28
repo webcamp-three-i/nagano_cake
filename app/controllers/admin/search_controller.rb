fd204495947612a4dbb35db47d17378772b804df
Hiroki Isobe <vagrant@localhost.localdomain>
2020-09-28 01:19:17 +0000

    [Fix]customer-search

--- app/controllers/admin/search_controller.rb
+++ app/controllers/admin/search_controller.rb
@@ -5,13 +5,23 @@ class Admin::SearchController < ApplicationController
     @content = params["search"]["content"]
     @model = params["search"]["model"]
     @method = params["search"]["method"]
-    @records = search_for(@content, @model, @method) #検索するというメソッドの引数にして代入 
+    @records = search_for(@content, @model, @method) #検索するというメソッドの引数にして代入
   end
-
+  
   private
-
+  
   def search_for(content, model, method) #上記のsearch_forメソッドを定義する。
-    if model == 'product'
+    if model == 'customer'
+      if method == 'perfect'
+        Customer.where('first_name LIKE ? OR last_name LIKE ?', content, content )
+      elsif  method == 'forward'
+        Customer.where('first_name LIKE ? OR last_name LIKE ?' , content+'%' , content+'%' )
+      elsif  method == 'backward'
+        Customer.where('first_name LIKE ? OR last_name LIKE ?' ,'%'+content , '%'+content )
+      else
+        Customer.where('first_name LIKE ? OR last_name LIKE ?' , '%'+content+'%', '%'+content+'%' )
+      end
+    elsif model == 'product'
       if method == 'perfect'
         Product.where('name LIKE ?' ,content)
       elsif method == 'forward'
@@ -22,5 +32,5 @@ class Admin::SearchController < ApplicationController
         Product.where('name LIKE ?' , '%'+content+'%' )
       end
     end
-  end
+  end  
 end
